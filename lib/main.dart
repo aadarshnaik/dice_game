63eb4efa415ba192c1fef145eb5165b7ee214510
aadarshnaik <aadarshnaik6@gmail.com>
2022-02-09 12:36:57 +0530

    Dice Game Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -16,9 +18,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNum = 1;
+  int rightDiceNum = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: (){
+                  setState(() {
+                    leftDiceNum = Random().nextInt(6)+1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNum.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                child: Image.asset('images/dice$rightDiceNum.png'),
+                onPressed: (){
+                  setState(() {
+                    rightDiceNum = Random().nextInt(6)+1;
+                  });
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
